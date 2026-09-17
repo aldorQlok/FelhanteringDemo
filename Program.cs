c0696be56a90244d54f3d344fa4ea10bf07a7506
aldorQlok <aldor.besher@qlok.se>
2026-09-17 11:21:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Felhantering
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
